1a306cb35e4b33b7176ad21d27bb11eaa0174468
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-01-13 11:37:59 +0300

    payment2

--- ItServiceApp/Controllers/PaymentController.cs
+++ ItServiceApp/Controllers/PaymentController.cs
@@ -18,5 +18,19 @@ namespace ItServiceApp.Controllers
 
             return View();
         }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult CheckInstallment(string binNumber)
+        {
+            if (binNumber.Length != 6)
+                return BadRequest(new
+                {
+                    Message = "Bad req."
+                });
+
+            var result = _paymentService.CheckInstallments(binNumber, 1000);
+            return Ok(result);
+        }
     }
 }
--- ItServiceApp/Services/IyzicoPaymentService.cs
+++ ItServiceApp/Services/IyzicoPaymentService.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using AutoMapper;
 using ItServiceApp.Models;
 using ItServiceApp.Models.Payment;
 using Iyzipay.Model;
+using Iyzipay.Request;
 using Microsoft.Extensions.Configuration;
+using MUsefulMethods;
 
 namespace ItServiceApp.Services
 {
@@ -11,9 +15,11 @@ namespace ItServiceApp.Services
     {
         private readonly IConfiguration _configuration;
         private readonly IyzicoPaymentOptions _options;
-        public IyzicoPaymentService(IConfiguration configuration)
+        private readonly IMapper _mapper;
+        public IyzicoPaymentService(IConfiguration configuration, IMapper mapper)
         {
             _configuration = configuration;
+            _mapper = mapper;
             var section = _configuration.GetSection(IyzicoPaymentOptions.Key);
             _options = new IyzicoPaymentOptions()
             {
@@ -24,10 +30,36 @@ namespace ItServiceApp.Services
             };
         }
 
-
-        public List<InstallmentModel> CheckInstallments(string binNumber, decimal price)
+        private string GenerateConversationId()
         {
-            
+            return StringHelpers.GenerateUniqueCode();
+        }
+
+        public InstallmentModel CheckInstallments(string binNumber, decimal price)
+        {
+            var conversationId = GenerateConversationId();
+            var request = new RetrieveInstallmentInfoRequest
+            {
+                Locale = Locale.TR.ToString(),
+                ConversationId = conversationId,
+                BinNumber = binNumber,
+                Price = price.ToString(new CultureInfo("en-US")),
+            };
+
+            var result = InstallmentInfo.Retrieve(request, _options);
+            if (result.Status == "failure")
+            {
+                throw new Exception(result.ErrorMessage);
+            }
+
+            if (result.ConversationId != conversationId)
+            {
+                throw new Exception("Hatalı istek oluturuldu");
+            }
+
+            InstallmentModel resultModel = _mapper.Map<InstallmentModel>(result.InstallmentDetails[0]);
+
+            Console.WriteLine();
             return null;
         }
 
