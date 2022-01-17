6c34361d44ebc883cf64a7e31a49c3dfe18be0bb
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-01-17 11:58:22 +0300

    .

---
+++ ItServiceApp/Areas/Admin/Controllers/ManageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItServiceApp.Areas.Admin.Controllers
+{
+    public class ManageController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
