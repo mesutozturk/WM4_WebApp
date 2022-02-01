4c05b1790ca3a768778a8612abda244c6fe51f63
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2022-02-01 10:01:22 +0300

    .

---
+++ ItServiceApp/Components/PricingTableViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using AutoMapper;
+using ItServiceApp.Data;
+using ItServiceApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ItServiceApp.Components
+{
+    public class PricingTableViewComponent : ViewComponent
+    {
+        private readonly MyContext _dbContext;
+        private readonly Mapper _mapper;
+
+        public PricingTableViewComponent(MyContext dbContext, Mapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var data = _dbContext.SubscriptionTypes
+                .ToList()
+                .Select(x => _mapper.Map<SubscriptionTypeViewModel>(x))
+                .OrderBy(x => x.Price)
+                .ToList();
+
+            return View(data);
+        }
+    }
+}
